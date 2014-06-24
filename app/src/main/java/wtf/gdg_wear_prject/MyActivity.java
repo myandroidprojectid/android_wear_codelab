28bf9cfc40a5d183b5d8fb290fb9fd5ab6998752
Saúl <saulmm2@gmail.com>
2014-06-24 18:58:14 +0200

    Added action and background to the simple notification

--- app/src/main/java/wtf/gdg_wear_prject/MyActivity.java
+++ app/src/main/java/wtf/gdg_wear_prject/MyActivity.java
@@ -1,6 +1,10 @@
 package wtf.gdg_wear_prject;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preview.support.v4.app.NotificationManagerCompat;
 import android.support.v4.app.NotificationCompat;
@@ -22,9 +26,16 @@ public class MyActivity extends Activity {
     }
 
     public void send_simple_notification (View v) {
+        Intent directoryEvent = new Intent(Intent.ACTION_VIEW);
+        directoryEvent.setData(Uri.parse("https://developers.google.com/events/5826146929213440/"));
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, directoryEvent, 0);
+
         NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(this)
                 .setContentText("Acabo de llegar al evento de Android Wear")
                 .setContentTitle("GDG Vigo")
+                .addAction(R.drawable.icon_event, "Aqui voy", pendingIntent)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.back_gdg))
                 .setSmallIcon(R.drawable.icon_gdg);
 
         nManager.notify(SIMPLE_NOT, nBuilder.build());
